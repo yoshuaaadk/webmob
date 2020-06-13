d5f0556a70acace97e9de69b6caa8297362fb977
yoshuaaadk <65962962+yoshuaaadk@users.noreply.github.com>
2020-06-13 11:13:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('activate', function(event) {
     caches.keys()
     .then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+        if (key !== CACHE_STATIC_NAME || key !== CACHE_DYNAMIC_NAME) {
           console.log('[Service Worker] Removing old cache.', key);
           return caches.delete(key);
         }
