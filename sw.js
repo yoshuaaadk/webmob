a6dabbea2080f089b4c879dd0e7f0c9d207b8892
yoshuaaadk <65962962+yoshuaaadk@users.noreply.github.com>
2020-06-13 10:34:48 +0700

    Update sw.js
    chacing

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-var CACHE_STATIC_NAME = 'static-v3';
-var CACHE_DYNAMIC_NAME = 'dynamic-v3';
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v4';
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -11,6 +11,7 @@ self.addEventListener('install', function(event) {
       console.log('[Service Worker] Precaching App Shell');
       cache.addAll([
         '/',
+        'index.html',
         '/js/app.js',
         '/js/fetch.js',
         '/css/style.css',
